30e1a18fa33664f0ca20247fe14a234fe01a1a58
Zayber <zayber.re@gmail.com>
2022-11-08 21:03:06 -0500

    Fix SignUp lag

--- app/src/main/java/com/ntn/taller3/composables/auth/SignUpScreen.kt
+++ app/src/main/java/com/ntn/taller3/composables/auth/SignUpScreen.kt
@@ -4,6 +4,7 @@ import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
@@ -11,10 +12,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
@@ -26,6 +24,9 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.ntn.taller3.composables.common.DialogBoxLoading
 import com.ntn.taller3.composables.common.TitledTextField
 import com.ntn.taller3.composables.navigation.Screens
@@ -34,7 +35,7 @@ import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun SignUpScreen(navController: NavController,_viewModel: SignUpViewModel = viewModel()) {
+fun SignUpScreen(navController: NavController, _viewModel: SignUpViewModel = viewModel()) {
     val firstname by _viewModel.firstname.collectAsState()
     val lastname by _viewModel.lastname.collectAsState()
     val email by _viewModel.email.collectAsState()
@@ -50,7 +51,6 @@ fun SignUpScreen(navController: NavController,_viewModel: SignUpViewModel = view
         "Tarjeta de identidad",
         "Tarjeta de extranjería"
     )
-    val bitmap = _viewModel.bitmap.collectAsState()
 
     val galleryLauncher = rememberLauncherForActivityResult(
         contract =
@@ -68,8 +68,7 @@ fun SignUpScreen(navController: NavController,_viewModel: SignUpViewModel = view
     if (isLoading) {
         DialogBoxLoading()
     }
-    Scaffold(modifier = Modifier.padding(20.dp), scaffoldState = scaffoldState) {
-        padding_values ->
+    Scaffold(modifier = Modifier.padding(20.dp), scaffoldState = scaffoldState) { padding_values ->
         Column(
             modifier = Modifier.padding(padding_values),
             horizontalAlignment = Alignment.CenterHorizontally
@@ -165,27 +164,7 @@ fun SignUpScreen(navController: NavController,_viewModel: SignUpViewModel = view
 
                     Spacer(modifier = Modifier.height(12.dp))
 
-                    image?.let {
-                        if (Build.VERSION.SDK_INT < 28) {
-                            _viewModel.setBitmap(
-                                MediaStore.Images
-                                    .Media.getBitmap(context.contentResolver, it)
-                            )
-
-                        } else {
-                            val source = ImageDecoder
-                                .createSource(context.contentResolver, it)
-                            _viewModel.setBitmap(ImageDecoder.decodeBitmap(source))
-                        }
-
-                        bitmap.value?.let { btm ->
-                            Image(
-                                bitmap = btm.asImageBitmap(),
-                                contentDescription = null,
-                                modifier = Modifier.size(400.dp)
-                            )
-                        }
-                    }
+                    AsyncImage(model = image, contentDescription = "")
                 }
 
                 TitledTextField(
